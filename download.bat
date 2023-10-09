d74d0738cefe0188a2302d5ecba927e19d27b70c
kairi003 <kairi.satellite@gmail.com>
2023-10-10 03:33:20 +0900

    update

--- download.bat
+++ download.bat
@@ -2,7 +2,7 @@
 
 for /f "tokens=1,2" %%a in (map.txt) do (
     if not exist "%%~dpb" mkdir "%%~dpb"
-    curl -k -o "%%b" -H "Referer:https://www.pixiv.net/" "%%a"
+    curl --ssl-no-revoke -o "%%b" -H "Referer:https://www.pixiv.net/" "%%a"
 )
 
 echo Done.
