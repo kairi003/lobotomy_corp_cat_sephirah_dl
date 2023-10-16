96afdfd06c82931835f721c294111f5956b2a90d
kairi003 <kairi.satellite@gmail.com>
2023-10-17 01:00:29 +0900

    fix

--- prefix.bat
+++ prefix.bat
@@ -3,8 +3,11 @@
 set /p prefix="Input prefix: "
 
 for /r %%a in (*.png) do (
-    echo rename "%%a" "%prefix%_%%~nxa"
-    rename "%%a" "%prefix%_%%~nxa"
+    echo rename "%%a" "%prefix%_%%~nxa.tmp"
+    rename "%%a" "%prefix%_%%~nxa.tmp"
 )
-
+for /r %%a in (*.png.tmp) do (
+    echo rename "%%a" "%%~na"
+    rename "%%a" "%%~na"
+)
 echo Done.
